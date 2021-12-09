fd214332926a77892647765dd848cf93a54b15a4
TJ <michael.btj@hotmail.com>
2021-12-09 16:31:22 +0000

    remove unused helper function and cleared duplicates of used ones

--- express_server.js
+++ express_server.js
@@ -45,55 +45,6 @@ const database = {
 }
 
 
-// === HELPER FUNCTIONS ===
-
-// GENERATE NEW SERIES OF RANDOM CHARACTERS
-const randomStr = function generateRandomString() {
-  const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
-  let newStr = '';
-  const charLength = 6;
-
-  // function gets random index
-  const index = () => Math.floor(Math.random() * characters.length);
-
-  // build string
-  for (let i = 0; i < charLength; i++) {
-    newStr += characters[index()];
-  }
-  
-  return newStr;
-}
-
-
-// // CHECK EMAIL
-// const getUserByEmail = function GetUserFromUserDatabaseByEmail(email, database) {
-//   for (const userID in database) {
-//     if (database[userID].email === email) {
-//       return userID;
-//     }
-//   }
-//   return null;
-// }
-
-// // BOOLEAN FOR OWNERSHIP OVER URL
-// const urlForUser = function urlOwnedByUser(userID, urlID) {
-//   return urlDatabase[urlID].userID === userID;
-// }
-
-
-// FILTER URLDATABASE FOR USER
-const urlsForUser = function urlsOwnedByUser(id) {
-  let userURLs = {};
-
-  for (const url in urlDatabase) {
-    if (urlForUser(id, url)) {
-      userURLs[url] = urlDatabase[url];
-    }
-  }
-  return userURLs;
-}
-
-
 // === GET REQUESTS ===
 // ROOT PAGE
 app.get("/", (req, res) => {
