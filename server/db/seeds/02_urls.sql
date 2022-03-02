b3ac225086f2517d0f61d076e4a7a209004c2709
TJ <michael.btj@hotmail.com>
2022-03-02 17:01:37 -0500

    update seeds

---
+++ server/db/seeds/02_urls.sql
@@ -0,0 +1,5 @@
+INSERT INTO urls (id, longurl, user_id) VALUES ("b2xVn2", "http://www.lighthouselabs.ca", 1);
+INSERT INTO urls (id, longurl, user_id) VALUES ("9sm5xK", "http://www.google.com", 2);
+
+
+
